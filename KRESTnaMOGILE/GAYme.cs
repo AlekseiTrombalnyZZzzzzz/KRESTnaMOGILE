e84396c1c719efe9b9a7ea169477c76f66f31010
user <user@WIN-AC5S22HU1K6>
2024-05-07 21:09:15 +0300

    Добавьте файлы проекта.

---
+++ KRESTnaMOGILE/GAYme.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KRESTnaMOGILE
+{
+    internal class GAYme
+    {
+        private string[] symbol = { "X", "O" };
+        private int steps = 0;
+        private player[] pl = { new player("X"), new player("O")};
+        public void Click(Button but, int n, Label lbl)
+        {
+            if (but.Text != "") return;
+            int step = steps % symbol.Length;
+            lbl.Text = pl[step].setcombo(n);
+            steps++;
+            but.Text = symbol[step];
+        }
+    }
+}
